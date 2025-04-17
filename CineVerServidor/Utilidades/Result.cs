a4d160d0b7d2e0cc8454eeea56875ab786a543da
Gabriel Armas <armasgabriel29@gmail.com>
2025-04-16 18:31:37 -0600

    add: Utilities project and result pattern have been created

---
+++ CineVerServidor/Utilidades/Result.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades
+{
+    public class Result<T>
+    {
+        public T Valor { get; private set; }
+        public bool EsExitoso { get; private set; }
+        public string Error { get; private set; }
+
+        public Result(T valor, bool esExitoso, string error)
+        {
+            Valor = valor;
+            EsExitoso = esExitoso;
+            Error = esExitoso ? string.Empty : error;
+        }
+
+        public Result(bool esExitoso, string error)
+        {
+            EsExitoso = esExitoso;
+            Error = esExitoso ? string.Empty : error;
+        }
+
+        public static Result<T> Exito(T value)
+        {
+            return new Result<T>(value, true, string.Empty);
+        }
+
+        public static Result<T> Fallo(string error)
+        {
+            return new Result<T>(default, false, error);
+        }
+    }
+}
