e64e458dfb64f7768deaec47735886796a25497b
Gabriel Armas <armasgabriel29@gmail.com>
2025-04-20 22:12:08 -0600

    feat: Confirmation window has been added on AgregarProductoDulceria

--- CineVerCliente/ModeloVista/AgregarProductoDulceriaModeloVista.cs
+++ CineVerCliente/ModeloVista/AgregarProductoDulceriaModeloVista.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace CineVerCliente.ModeloVista
@@ -14,12 +15,15 @@ namespace CineVerCliente.ModeloVista
         private float _costoUnitario;
         private float _precioVentaUnitario;
         private byte[] _imagenProducto;
+        private Visibility _mostrarMensajeCancelarOperacion = Visibility.Collapsed;
 
         private readonly MainWindowModeloVista _mainWindowModeloVista;
 
         public ICommand ConfirmarCambiosComando { get; }
-        public ICommand AgregarNuevoProducto { get; }
+        public ICommand AgregarNuevoProductoComando { get; }
         public ICommand CancelarComando { get; }
+        public ICommand ConfirmarCancelacionComando { get; }
+        public ICommand CancelarCancelacionComando { get; }
 
         public string NombreProducto
         {
@@ -69,6 +73,49 @@ namespace CineVerCliente.ModeloVista
                 _imagenProducto = value;
                 OnPropertyChanged();
             }
+        }
+
+        public Visibility MostrarMensajeCancelarOperacion
+        {
+            get { return _mostrarMensajeCancelarOperacion; }
+            set
+            {
+                _mostrarMensajeCancelarOperacion = value;
+                OnPropertyChanged(nameof(MostrarMensajeCancelarOperacion));
+            }
+        }
+
+        public AgregarProductoDulceriaModeloVista(MainWindowModeloVista mainWindowModeloVista)
+        {
+            _mainWindowModeloVista = mainWindowModeloVista;
+            ConfirmarCambiosComando = new ComandoModeloVista(ConfirmarCambios);
+            AgregarNuevoProductoComando = new ComandoModeloVista(AgregarNuevoProducto);
+            CancelarComando = new ComandoModeloVista(CancelarOperacion);
+            ConfirmarCancelacionComando = new ComandoModeloVista(ConfirmarCancelacion);
+            CancelarCancelacionComando = new ComandoModeloVista(CancelarCancelacion);
+        }
+
+        private void ConfirmarCambios(object obj)
+        {
+        }
+
+        private void AgregarNuevoProducto(object obj)
+        {
+        }
+
+        private void CancelarOperacion(object obj)
+        {
+            MostrarMensajeCancelarOperacion = Visibility.Visible;
+        }
+
+        private void ConfirmarCancelacion(object obj)
+        {
+            MostrarMensajeCancelarOperacion = Visibility.Collapsed;
+        }
+
+        private void CancelarCancelacion(object obj)
+        {
+            MostrarMensajeCancelarOperacion = Visibility.Collapsed;
         }
     }
 }
