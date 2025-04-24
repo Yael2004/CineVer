f995c372ce7a5ac48897edec4e13b4190d5c3035
Luis Angel <luisangel040504@gmail.com>
2025-04-24 17:13:16 -0600

    Tercera actualización server

--- CineVerServidor/DAO/PelículaDAO.cs
+++ CineVerServidor/DAO/PelículaDAO.cs
@@ -43,10 +43,14 @@ namespace DAO
             {
                 try
                 {
-                    var pelicula = entities.Película.Where(e => e.director.Equals(director) && e.nombre.Equals(nombrePelicula));
+                    var pelicula = entities.Película.Where(e => e.director.Equals(director) && e.nombre.Equals(nombrePelicula)).FirstOrDefault();
                     if(pelicula != null)
                     {
-
+                        return Result<bool>.Exito(true);
+                    }
+                    else
+                    {
+                        return Result<bool>.Exito(false);
                     }
                 }
                 catch (DbEntityValidationException ex)
@@ -58,6 +62,131 @@ namespace DAO
                     return Result<bool>.Fallo(sqlEx.Message);
                 }
             }
-        } 
+        }
+        public Result<string> AgregarPelicula(Película pelicula)
+        {
+            using (CineVerEntities entities = new CineVerEntities())
+            {
+                try
+                {
+                    if(ExistePelicula(pelicula.nombre, pelicula.director).Valor)
+                    {
+                        return Result<string>.Fallo("La película ya ha sido agregada previamente");
+                    }
+                    else
+                    {
+                        entities.Película.Add(pelicula);
+                        entities.SaveChanges();
+                        return Result<string>.Exito("Pelicula agregada exitosamente");
+                    }
+                }
+                catch(DbEntityValidationException ex)
+                {
+                    return Result<string>.Fallo(ex.Message);
+                }
+                catch(SqlException sqlEx)
+                {
+                    return Result<string>.Fallo(sqlEx.Message);
+                }
+            }
+        }
+        public Result<int> ObtenerIdPelicula(string nombre, string director)
+        {
+            using (CineVerEntities entities = new CineVerEntities())
+            {
+                try
+                {
+                    var pelicula = entities.Película.Where(e=>e.nombre.Equals(nombre) && e.director.Equals(director)).FirstOrDefault();
+                    return Result<int>.Exito(pelicula.idPelicula);
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    return Result<int>.Fallo(ex.Message);
+                }
+                catch (SqlException sqlEx)
+                {
+                    return Result<int>.Fallo(sqlEx.Message);
+                }
+            }
+        }
+        public Result<String> EditarPelicula(Película peliculaEditada, Película peliculaOriginal)
+        {
+            using (CineVerEntities entities = new CineVerEntities())
+            {
+                try
+                {
+                    int idPelicula = ObtenerIdPelicula(peliculaOriginal.nombre,peliculaOriginal.director).Valor;
+                    Película peliculaNueva = entities.Película.Find(idPelicula);
+                    if (peliculaNueva != null)
+                    {
+                        peliculaNueva.nombre = peliculaEditada.nombre;
+                        peliculaNueva.director = peliculaEditada.director;
+                        peliculaNueva.duracion = peliculaEditada.duracion;
+                        peliculaNueva.genero = peliculaEditada.genero;
+                        peliculaNueva.sinopsis = peliculaEditada.sinopsis;
+                        entities.SaveChanges();
+                        return Result<string>.Exito("Pelicula editada exitosamente");
+                    }
+                    else
+                    {
+                        return Result<String>.Fallo("No se encontro la pelicula");
+                    }
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    return Result<string>.Fallo(ex.Message);
+                }
+                catch (SqlException sqlEx)
+                {
+                    return Result<string>.Fallo(sqlEx.Message);
+                }
+            }
+        }
+        public Result<string> EliminarPelicula(Película pelicula)
+        {
+            using (CineVerEntities entities = new CineVerEntities())
+            {
+                try
+                {
+                    var peliculaEliminar = entities.Película.Find(ObtenerIdPelicula(pelicula.nombre,pelicula.director).Valor);
+                    if(peliculaEliminar != null)
+                    {
+                        entities.Película.Remove(peliculaEliminar);
+                        entities.SaveChanges();
+                        return Result<string>.Exito("Pelicula eliminada exitosamente");
+                    }
+                    else
+                    {
+                        return Result<string>.Fallo("Pelicula no encontrada");
+                    }
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    return Result<string>.Fallo(ex.Message);
+                }
+                catch (SqlException sqlEx)
+                {
+                    return Result<string>.Fallo(sqlEx.Message);
+                }
+            }
+        }
+        public Result<List <Película>> ObtenerPeliculasPorNombre(int idSucursal, string nombre)
+        {
+            using (CineVerEntities entities = new CineVerEntities())
+            {
+                try
+                {
+                    List <Película> listaPeliculas = 
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    return Result < List < Película >>.Fallo(ex.Message);
+                }
+                catch (SqlException sqlEx)
+                {
+                    return Result< List<Película>>.Fallo(sqlEx.Message);
+                }
+            }
+        }
     }
 }
