06732c2ba291aa75cd9f7cb82f87e5204be4f41c
Gabriel Armas <armasgabriel29@gmail.com>
2025-04-26 11:29:59 -0600

    add: Reportar merma CU view files were added

---
+++ CineVerCliente/Modelo/ProductoDulceria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineVerCliente.Modelo
+{
+    public class ProductoDulceria
+    {
+        private string _costoUnitario;
+        public string CostoUnitario
+        {
+            get { return _costoUnitario; }
+            set
+            {
+                _costoUnitario = value.Replace("$", "");
+                _costoUnitario = $"$ {_costoUnitario}";
+            }
+        }
+        private string _precioVentaUnitario;
+        public string PrecioVentaUnitario
+        {
+            get { return _precioVentaUnitario; }
+            set
+            {
+                _precioVentaUnitario = value.Replace("$", "");
+                _precioVentaUnitario = $"$ {_precioVentaUnitario}";
+            }
+        }
+        public byte[] Imagen { get; set; }
+        public string Nombre { get; set; }
+        public string CantidadInventario { get; set; }
+    }
+}
---
+++ CineVerCliente/ModeloVista/CantidadMermaModeloVista.cs
@@ -0,0 +1,22 @@
+﻿using CineVerCliente.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineVerCliente.ModeloVista
+{
+    public class CantidadMermaModeloVista : BaseModeloVista
+    {
+        public ProductoDulceria Producto { get; set; }
+
+        private readonly MainWindowModeloVista _mainWindowModeloVista;
+
+        public CantidadMermaModeloVista(MainWindowModeloVista mainWindowModeloVista, ProductoDulceria producto)
+        { 
+            _mainWindowModeloVista = mainWindowModeloVista;
+            Producto = producto;
+        }
+    }
+}
