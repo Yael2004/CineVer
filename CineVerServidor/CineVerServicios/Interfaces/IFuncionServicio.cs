892c3a340e62075094671de056056ae2b094cb36
Luis Angel <luisangel040504@gmail.com>
2025-05-03 10:44:25 -0600

    Servicios funcion completos

--- CineVerServidor/CineVerServicios/Interfaces/IFuncionServicio.cs
+++ CineVerServidor/CineVerServicios/Interfaces/IFuncionServicio.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using CineVerServicios.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CineVerServicios.Interfaces
 {
-    internal interface Interface1
+    [ServiceContract]
+    
+    internal interface IFuncionServicio
     {
+        [OperationContract]
+        ListaFuncionesDTO ObtenerFuncionesPorPeliculaYFecha(int idPelicula, DateTime fecha);
+        [OperationContract]
+        ListaFuncionesDTO ObtenerFuncionesPorFecha(DateTime fecha);
+        [OperationContract]
+        ListaFuncionesDTO ObtenerFuncionesPorFechaYSala(int idSala, DateTime fecha);
+        [OperationContract]
+        string AgregarFuncion(FuncionDTO funcion);
+        [OperationContract]
+        string EditarFUncion(FuncionDTO funcionOriginal, FuncionDTO funcionEditada);
+        [OperationContract]
+        string EliminarFuncion(FuncionDTO funcion);
+
     }
 }
