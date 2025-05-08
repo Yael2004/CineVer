f9252769ace997630032e22fe14899112750f915
Lopez Garcia Luis Angel <zs22013626@estudiantes.uv.mx>
2025-05-08 13:00:27 -0600

    Inicio de funciones

---
+++ CineVerCliente/ModeloVista/AgregarFuncionModeloVista.cs
@@ -0,0 +1,62 @@
+﻿using CineVerCliente.FuncionServicio;
+using CineVerCliente.PeliculaServicio;
+using CineVerCliente.SalaServicio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CineVerCliente.ModeloVista
+{
+    public class AgregarFuncionModeloVista : BaseModeloVista
+    {
+        private readonly MainWindowModeloVista _mainWindowModeloVista;
+        public ICommand GuardarCommand { get; }
+        public ICommand CancelarCommand { get; }
+        private string _nombrePelicula;
+        public string NombrePelicula
+        {
+            get => _nombrePelicula;
+            set
+            {
+                _nombrePelicula = value;
+                OnPropertyChanged(nameof(NombrePelicula));
+
+            }
+        }
+
+        private string _nombreSala;
+        public string NombreSala
+        {
+            get => _nombreSala;
+            set
+            {
+                _nombreSala = value;
+                OnPropertyChanged(nameof(NombreSala));
+
+            }
+        }
+
+        private SalaServicioClient _salaServicio = new SalaServicioClient();
+        private PelículaServicioClient _peliculaServicio = new PelículaServicioClient();
+        private FuncionServicioClient _funcionServicio = new FuncionServicioClient();
+
+        public AgregarFuncionModeloVista(MainWindowModeloVista mainWindowModeloVista)
+        {
+            _mainWindowModeloVista = mainWindowModeloVista;
+            GuardarCommand = new ComandoModeloVista(Guardar);
+            CancelarCommand = new ComandoModeloVista(Regresar);
+
+        }
+        private void Guardar(Object obj)
+        {
+
+        }
+        private void Regresar(Object obj)
+        {
+
+        }
+    }
+}
