3c6159bd3a6aa54da88cc665f728244f0e378490
Lopez Garcia Luis Angel <luisangel040504gmail.com>
2025-05-11 00:53:31 -0600

    Parte de Luis Terminada

---
+++ CineVerCliente/Helpers/TimeSpanToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CineVerCliente.Helpers
+{
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan ts)
+                return ts.ToString(@"hh\:mm"); // Muestra en formato 24h como "14:30"
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (TimeSpan.TryParse(value?.ToString(), out var result))
+                return result;
+            return TimeSpan.Zero;
+        }
+    }
+
+}
